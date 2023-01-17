2f661d15fd14a12b42ef19341e4a684a1b92cc0c
JustWarlus <justwarlus@gmail.com>
2023-01-17 03:22:51 +0300

    feat (pages): create trends and favorites pages

---
+++ src/components/FavoritesEmpty/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledFavoriteEmpty = styled.div`
+  min-height: 740px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export { StyledFavoriteEmpty };
